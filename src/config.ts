5a496f3447efa2cec47373327d1645e9833d212e
Cole <46254944+Cole404@users.noreply.github.com>
2025-02-12 21:59:38 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,9 +54,14 @@ export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/home_avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Kamiyd',
   bio: '路漫漫其修远兮，吾将上下而求索',
-link:[
-  {
-  },
+links: [
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/Lapis0x0',
+    },
+  ],
+}
 export const licenseConfig: LicenseConfig = {
   enable: true,
   name: 'CC BY-NC-SA 4.0',
